cd15ead43fa96f35062554c41c20dd9d033c4e3a
Artian RIKA <artianrika@gmail.com>
2024-05-24 22:43:05 +0200

    p

--- api/server.js
+++ api/server.js
@@ -2,23 +2,26 @@ var express = require('express')
 var app = express()
 const PORT = 3000
 
-var list = [{"elo": "there"}]
+var list = [
+    { 
+        "id": "00",
+        "lat": 0,
+        "lng": 0
+    }
+]
 
 app.get('/', (req, res) =>{
     res.json(list)
 })
 
-app.post('/locations/:id/:lat/:lng/', (req, res) =>{
+app.post('/locations/:id', (req, res) =>{
     const id = req.params.id
-    const lat = re.params.lat 
-    const lng = re.params.lng 
     const newData = req.body
 
-    console.log(newData)
 
-    list.push({id, lat,lng})
+    list.push({id, newData})
 
-    res.status(201).json({ message: 'Data added successfully', list });
+    res.status(201).json({ message: 'Data added successfully', newData });
 })
 
 app.listen(PORT, ()=>{
