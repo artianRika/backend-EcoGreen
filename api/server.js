8e709322994f6ff42fdf27a6878941afd71eccd7
Artian RIKA <artianrika@gmail.com>
2024-05-26 16:28:59 +0200

    ready

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-//exp
+//expe
 app.use((req, res, next) => {
     // Allow requests from any origin
   res.setHeader('Access-Control-Allow-Origin', '*');
