5e026305a7e559fe4b5b96b0f0a563e38603b5f6
DorantVelija <dorantvelija@outlook.com>
2024-05-26 10:27:10 +0200

    last functioning version

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-//exp b
+//exp 
 app.use((req, res, next) => {
     // Allow requests from any origin
   res.setHeader('Access-Control-Allow-Origin', '*');
